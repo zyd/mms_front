6924054d481ba43dfb65ff39a8d476ecb989f8aa
zhaoyidong <zhaoyidongzdd@foxmail.com>
2020-05-11 04:43:21 +0800

    同步yanhai项目样式

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -29,9 +29,7 @@ import '@/permission' // permission control
 // }
 
 // set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI, { size: 'small', locale })
 
 Vue.config.productionTip = false
 
